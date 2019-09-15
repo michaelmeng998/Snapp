b7a7dbbea351c794b13924c9982fc0cad8a0e25c
Michael Meng <michael_meng@Michaels-MacBook-Pro.local>
2019-09-15 09:42:16 -0400

    commit after finish MVP

--- components/AR_Camera.js
+++ components/AR_Camera.js
@@ -4,13 +4,17 @@ import {
   View,
   TouchableOpacity,
   FlatList,
-  CameraRoll
+  CameraRoll,
+  Button
 } from "react-native";
 import * as takeSnapshotAsync from "expo";
 import * as Permissions from "expo-permissions";
 import * as ImageManipulator from "expo-image-manipulator";
 import { Camera } from "expo-camera";
 import * as FileSystem from "expo-file-system";
+
+const webUrl =
+  "https://stancarey.files.wordpress.com/2013/03/futurama-fry-should-i-lol-or-roflmao.jpg";
 
 const Clarifai = require("clarifai");
 
@@ -64,48 +68,72 @@ export default class App extends React.Component {
     let photo = await this.capturePhoto();
     let resized = await this.resize(photo);
     let predictions = await this.predict(resized);
-    // let predictions = await this.predict(photo);
-    this.setState({ predictions: predictions.outputs[0].data.concepts });
+    console.log("predictions1...");
+    console.log(predictions.outputs[0].input.data.image.url);
 
-    //code for saving screenshot to camera gallery
-    console.log("Save to camera roll....");
-    this.downloadAsync;
-    this._saveToCameraRollAsync;
+    // let predictions = await this.predict(photo);
+    this.setState({ predictions: predictions.outputs[0].data.concepts[0] });
+    this.setState({ predictions: predictions.outputs[0].input.data.image });
+
+    // this._downloadAsync(predictions);
+    // this._saveToCameraRollAsync();
 
     //code for saving screenshot if confidence level is above a certain threshold
     console.log("Computed confidence levels:...");
-    // console.log(predictions.outputs[0]);
-    if (predictions.outputs[0].data.concepts[0].value > 0.85) {
+    console.log(predictions.outputs[0]);
+    if (predictions.outputs[0].data.concepts[0].value > 0.8) {
       console.log("Confidence level is above 0.85 and is: ");
-      console.log(predictions.outputs[0].data.concepts[0].value);
+      //   console.log(predictions.outputs[0].data.concepts[0].value);
+      //code for saving screenshot to camera gallery
+      console.log("predictions...");
+      //   console.log(predictions.outputs[0]);
+
+      console.log("Save to camera roll....");
+
+      console.log("Saved to disk....");
+      let result = await FileSystem.downloadAsync(
+        predictions.outputs[0].input.data.image.url,
+        FileSystem.documentDirectory + "pic.png"
+      );
+      this.setState({ localPath: result.uri });
+
+      console.log("Saved to CAMERA ROLL....");
+      try {
+        let result = await CameraRoll.saveToCameraRoll(
+          this.state.localPath,
+          "photo"
+        );
+        alert(JSON.stringify(result));
+        this.setState({ cameraRollPath: result });
+      } catch (e) {
+        alert(JSON.stringify(e));
+      }
     }
   };
 
   //code to save image to disk and then to camera roll
-  _downloadAsync = async () => {
-    console.log("Saved to disk....");
-    let result = await FileSystem.downloadAsync(
-      predictions.outputs[0].data.concepts.uri,
-      FileSystem.documentDirectory +
-        predictions.outputs[0].data.concepts.value +
-        "pic.png"
-    );
-    this.setState({ localPath: result.uri });
-  };
+  //   _downloadAsync = async () => {
+  //     console.log("Saved to disk....");
+  //     let result = await FileSystem.downloadAsync(
+  //       predictions.outputs[0].input.data.image.url,
+  //       FileSystem.documentDirectory + "pic.png"
+  //     );
+  //     this.setState({ localPath: result.uri });
+  //   };
 
-  _saveToCameraRollAsync = async () => {
-    console.log("Saved to CAMERA ROLL....");
-    try {
-      let result = await CameraRoll.saveToCameraRoll(
-        this.state.localPath,
-        "photo"
-      );
-      alert(JSON.stringify(result));
-      this.setState({ cameraRollPath: result });
-    } catch (e) {
-      alert(JSON.stringify(e));
-    }
-  };
+  //   _saveToCameraRollAsync = async () => {
+  //     console.log("Saved to CAMERA ROLL....");
+  //     try {
+  //       let result = await CameraRoll.saveToCameraRoll(
+  //         this.state.localPath,
+  //         "photo"
+  //       );
+  //       alert(JSON.stringify(result));
+  //       this.setState({ cameraRollPath: result });
+  //     } catch (e) {
+  //       alert(JSON.stringify(e));
+  //     }
+  //   };
 
   render() {
     const { hasCameraPermission, predictions } = this.state;
